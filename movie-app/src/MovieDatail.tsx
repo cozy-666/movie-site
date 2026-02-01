9217ecca90826bd21deeb40fe775c9315a1aae6e
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 16:10:22 +0900

    useParams パラメータ情報獲得

--- movie-app/src/MovieDatail.tsx
+++ movie-app/src/MovieDatail.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router";
+
 function MovieDetail() {
+    const { movieId } = useParams();
+    
     return (
         <>
         <h1>Movie Detail</h1>
+        <div>{movieId}</div>
         </>
     )
 };
