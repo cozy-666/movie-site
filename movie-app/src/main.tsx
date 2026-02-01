7e9eb1ea92dff9441b0ef3393223f959e0f4aa5d
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 16:08:39 +0900

    ルーティングの設定

--- movie-app/src/main.tsx
+++ movie-app/src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import MovieDetail from './MovieDatail.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {path: "/", Component: App},
+  {path: "/movies/:movieId", Component: MovieDetail},
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MovieDetail/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
